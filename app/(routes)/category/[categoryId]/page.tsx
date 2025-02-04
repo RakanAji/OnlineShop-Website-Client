2471def5dba471fb214ae36ede91761ebb574cfe
RakanAji <pratamarakanaji@gmail.com>
2025-02-04 17:08:48 +0700

    client v.1.1

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,3 +1,4 @@
+import getCategories from "@/actions/get-categories";
 import getCategory from "@/actions/get-category";
 import getProducts from "@/actions/get-products";
 import Banner from "@/components/banner";
@@ -11,34 +12,35 @@ interface CategoryPageProps{
     }
 }
 
-const CategoryPage: React.FC<CategoryPageProps> = async ({
-    params
-}) => {
-    const products = await getProducts({
-        categoryId: params.categoryId
-    })
+export async function generateStaticParams() {
+  // Jika Anda memiliki daftar kategori, dapatkan semua ID kategori untuk pre-rendering
+  // Contoh: Ambil daftar kategori dari API atau database
+  const categories = await getCategories(); // Anda perlu buat fungsi getCategories()
+  return categories.map((category) => ({
+    categoryId: category.id,
+  }));
+}
 
-    const category = await getCategory(params.categoryId)
+export default async function CategoryPage({ params }: CategoryPageProps) {
+  const products = await getProducts({ categoryId: params.categoryId });
+  const category = await getCategory(params.categoryId);
 
-    return ( 
-        <div className=" bg-white">
-            <Container>
-                <Banner
-                data={category.banner}
-                />
-                <div className=" px-4 sm:px-6 lg:px-8 pb-24">
-                    <div className=" mt-6 lg:col-span-4 lg:mt-0">
-                        {products.length === 0 && <NoResults />}
-                        <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                            {products.map((item) =>(
-                                <ProductCard key={item.id} data={item} />
-                            ))}
-                        </div>
-                    </div>
-                </div>
-            </Container>
+  return (
+    <div className=" bg-white">
+      <Container>
+        <Banner data={category.banner} />
+        <div className=" px-4 sm:px-6 lg:px-8 pb-24">
+          <div className=" mt-6 lg:col-span-4 lg:mt-0">
+            {products.length === 0 && <NoResults />}
+            <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+              {products.map((item) => (
+                <ProductCard key={item.id} data={item} />
+              ))}
+            </div>
+          </div>
         </div>
-     );
+      </Container>
+    </div>
+  );
 }
  
-export default CategoryPage ;
