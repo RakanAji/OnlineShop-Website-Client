35c4993bdcaa70a19b39db899626e1a9cf7c7740
RakanAji <pratamarakanaji@gmail.com>
2025-02-04 15:40:59 +0700

    client v.1

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBanners from "@/actions/get-banner";
+import getProducts from "@/actions/get-products";
+import Banner from "@/components/banner";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const banner = await getBanners("dc050db7-b3de-4dc8-8540-c1cc473f8fe9");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10 ">
+        <Banner data={banner} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8 ">
+          <ProductList title="Produk Unggulan" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
